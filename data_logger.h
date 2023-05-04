7b38ba77b7cb15272a177a5c6f4af3722d37eda8
Felipe Escorel <felipe.escorel@gmail.com>
2023-05-04 19:47:58 -0300

    :ghost:

--- data_logger.h
+++ data_logger.h
@@ -22,10 +22,27 @@ uint16_t buffer_1[BUFFER_LENGTH];
 volatile int buffer_0_index, buffer_1_index;
 
 void Init_ADC(){
-    // DEFINIR ADC (OLHAR DOCUMENTACAO)
+    ADCENC = 0
+    ADCCTL0 |= ADCSHT_2;            // 16 ADCCLK cycles
+    ADCCTL0 |= ADCON;               // ADC ON
+    ADCCTL1 |= ADCSHP;              // ample-and-hold pulse-mode select  ==>> ADCCLK = MODOSC; sampling timer
+    ADCCTL2 &= ~ADCRES;             // clear ADCRES in ADCCTL
+    ADCCTL2 |= ADCRES_2;            // 12-bit conversion results
+    // avaliar portas
+    ADCMCTL0 |= ADCINCH_3;          // ADC input Channel  Port 1.3
+    //  ADCMCTL0 |= ADCINCH_6;          // ADC input Channel  Port 1.6
+    
+    ADCIE |= ADCIE0;                // Enable the Interrupt request for a completed ADC_B conversion
+    ADCCTL0 |= ADCENC;              // Enable conversions
+    ADCCTL0 |= ADCSC;               // Start conversion
+    ADCCTL0 |= ADCMSC;
 
-    //
+    __bis_SR_register(LPM0_bits | GIE);                
+    __no_operation();
 
+    PMMCTL0_H = PMMPW_H;                                        
+    PMMCTL2 |= INTREFEN;  
+    ADCENC = 1
 }
 
 
